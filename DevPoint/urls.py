0b905c445463b06f70f4c71afe8282d061abb2e3
kayque <kayquespimentel1@gmail.com>
2026-09-18 23:53:45 -0300

    site

--- DevPoint/urls.py
+++ DevPoint/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from store.views import HomeView, StoreView, NewProductView, NewCategoryView
+from store.views import HomeView, StoreView, NewProductView, NewCategoryView, DetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
-    path('store', StoreView.as_view(), name='store'),
+    path('store/', StoreView.as_view(), name='store'),
+    path('store/<int:pk>', DetailView.as_view(), name='detail'),
     path('new-product/', NewProductView.as_view(), name='newproduct'),
     path('new-category/', NewCategoryView.as_view(), name='newcategory')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
